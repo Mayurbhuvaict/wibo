9d097f23e0379950e708e93b0f934097819d7a62
mitkhuntitc <mitkhunt@icreativetechnolabs.com>
2022-11-01 12:25:36 +0530

    SCHE-634 change email header footer

--- ZeobvCmsElements/src/ZeobvCmsElements.php
+++ ZeobvCmsElements/src/ZeobvCmsElements.php
@@ -2,16 +2,12 @@
 
 namespace Zeobv\CmsElements;
 
+
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Uuid\Uuid;
