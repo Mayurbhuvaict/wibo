7fd1e57c93166d1a0ab185e0871c1f69982a8039
smitb <smit_bhavsar@icreativetechnolabs.com>
2022-12-02 15:20:45 +0530

    Updated PLugins

--- ZeobvCmsElements/src/Core/Content/Media/Cms/ImageCaptionCmsElementResolver.php
+++ ZeobvCmsElements/src/Core/Content/Media/Cms/ImageCaptionCmsElementResolver.php
@@ -2,9 +2,17 @@
 
 namespace Zeobv\CmsElements\Core\Content\Media\Cms;
 
+use Shopware\Core\Content\Media\Cms\AbstractDefaultMediaResolver;
 use Shopware\Core\Content\Media\Cms\ImageCmsElementResolver;
 
 class ImageCaptionCmsElementResolver extends ImageCmsElementResolver{
+
+    private AbstractDefaultMediaResolver $mediaResolver;
+
+    public function __construct(AbstractDefaultMediaResolver $mediaResolver)
+    {
+        $this->mediaResolver = $mediaResolver;
+    }
 
     public function getType(): string
     {
